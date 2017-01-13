5cffe34b44aed3587baa03a937cbdd0c815850cb
kristjaningiragnarsson <kristjanra210@nemi.tskoli.is>
2017-01-13 12:44:16 +0000

    Add files via upload

---
+++ verkefni 1/part 2/script.js
@@ -0,0 +1,8 @@
+var a = 'banani';
+var b = 'pizza';
+var c = 'kjöt';
+var d = 'gos';
+var f = 'epli';
+var listi = a + "," + b + ","  + c + "," + d + "," + f;
+var el = document.getElementById('a');
+el.textContent = listi;
